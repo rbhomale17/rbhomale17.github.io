f0c1db3aee4f568771f8dd77bff2d5beb60410b3
rbhomale <rushikeshbhomale@gmail.com>
2023-05-20 00:32:05 +0530

    projects added to portfolio

--- index.js
+++ index.js
@@ -25,12 +25,6 @@ darkmode.onclick = () => {
     document.body.classList.remove("active");
   }
 };
-
-// src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js"
-
-// GitHubCalendar(".calendar", "Surajy001");
-// or enable responsive functionality
-// GitHubCalendar(".calendar", "Surajy001", { responsive: true });
       
 
 document.getElementById('resume-link-1').addEventListener("click", () => {
