39db305f4f3b2650804c72587efa5c38c36f3932
rbhomale <rushikeshbhomale@gmail.com>
2023-05-24 23:38:41 +0530

    navbar toggling added

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
- document.getElementById('resume-button-1').addEventListener("click", () => {
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document.getElementById('resume-button-1').addEventListener("click", () => {
   // console.log("hhiii1");
   window.open("https://drive.google.com/file/d/1UAj5f-fHiInGHRnJJjCvNHGGwzpm3RMo/view?usp=share_link", "_blank");
 });
