974342743b3b3b81825d8911e324a580e2b3c26f
rbhomale17 <rushikeshbhomale@gmail.com>
2023-10-20 18:50:19 +0530

    As per round 2 feedback from ayush sir portfolio updated

--- index.js
+++ index.js
@@ -21,25 +21,25 @@ document.getElementById('resume-button-1').addEventListener("click", () => {
 //   window.open("https://drive.google.com/file/d/1UAj5f-fHiInGHRnJJjCvNHGGwzpm3RMo/view?usp=share_link", "_blank");
 // });
 
-// owl carousel script 
-$('.carousel').owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplay: true,
-  autoplayTimeOut: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false
-    },
-    800: {
-      items: 2,
-      nav: false
-    },
-    // 1000: {
-    //   items: 3,
-    //   nav: false
-    // }
-  }
-});
+// // owl carousel script 
+// $('.carousel').owlCarousel({
+//   margin: 20,
+//   loop: true,
+//   autoplay: true,
+//   autoplayTimeOut: 2000,
+//   autoplayHoverPause: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//       nav: false
+//     },
+//     800: {
+//       items: 2,
+//       nav: false
+//     },
+//     // 1000: {
+//     //   items: 3,
+//     //   nav: false
+//     // }
+//   }
+// });
